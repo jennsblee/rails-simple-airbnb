46d6563baa6f2fadb6146981addbf6432412d8d9
Jennifer Lee <jlee970124@gmail.com>
2020-11-18 17:00:03 +0900

    simple airbnb done

--- Gemfile
+++ Gemfile
@@ -47,5 +47,10 @@ group :test do
   gem 'webdrivers'
 end
 
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.6.1'
+gem 'simple_form'
+gem 'faker'
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+5.times do
+  Flat.create!(
+    name: Faker::Address.community,
+    address: Faker::Address.full_address,
+    description: Faker::Movie.quote,
+    price_per_night: rand(75..350),
+    number_of_guests: rand(1..6),
+    image: ['https://cf.bstatic.com/images/hotel/max1024x768/155/155406086.jpg', 'https://q4g9y5a8.rocketcdn.me/wp-content/uploads/2020/02/home-banner-2020-02-min.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF-nZUnCukp6GetMRPEOLAntGHqLjEb2X1kQ&usqp=CAU', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFtpcsmOwRUl9kMrkiKacymNMUesD18aXSHg&usqp=CAU', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQwOxPgo1wQf86te2IsV1TqUS-o_izuxvcXA&usqp=CAU'].sample
+  )
+end
